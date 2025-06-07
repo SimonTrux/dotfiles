194d30499429f253bda86a477218e12784402bc8
SimonTrux <simon.truxillo@gmail.com>
2025-06-07 19:21:14 +0200

    build: neovim bin nvim install case

--- scripts/install_pkgs.sh
+++ scripts/install_pkgs.sh
@@ -12,7 +12,8 @@ fi
 
 # Packages to be installed.
 PKG_LIST="grep vim tmux fzf bat zoxide"
-EXTRA_PKG_LIST=" npm cargo"
+VIM_LSP_DEPS=" npm cargo"
+NEOVIM_PKG=" neovim make"
 
 # if "tput" is missing, add ncurses which provides it to PKG_LIST
 # This will trigger install in next block as "command -v ncuses" will fail.
@@ -21,14 +22,38 @@ if ! command -v tput > /dev/null 2>&1 ; then
   PKG_LIST+=" ncurses"
 fi
 
+echo "Do you want to install neovim with config ?"
+select choice in "yes" "no"; do
+	case $choice in
+		yes) PKG_LIST+=$NEOVIM_PKG ; break ;;
+		no) break ;;
+	esac
+done
+
+echo "Do you want to also install vim LSP dependencies : $VIM_LSP_DEPS ?"
+select choice in "yes" "no"; do
+	case $choice in
+		yes) PKG_LIST+=$VIM_LSP_DEPS ; break ;;
+		no) break ;;
+	esac
+done
+
 # Check If all packages are already installed.
 SKIP_INSTALL=true
+info "PKG_LIST = $PKG_LIST"
 for pkg in $PKG_LIST ; do
-  if ! command -v $pkg > /dev/null 2>&1 ; then
+  if ! command -v $pkg &> /dev/null && [ "$pkg" != "neovim" ] ; then
+    echo $pkg
+    info Some packages need installing.
+    SKIP_INSTALL=false
+    break
+  # edge case : neovim pkg / nvim binary
+  elif ! command -v nvim &> /dev/null && [ "$pkg" == "neovim" ] ; then
     info Some packages need installing.
     SKIP_INSTALL=false
     break
   fi
+
 done
 
 if $SKIP_INSTALL ; then
@@ -93,18 +118,10 @@ if [[ $EUID == 0 ]]
   then SUDO_CMD=""
 fi
 
-
 info "Running $OS os | $ID."
 info "Installation planned with : $SUDO $PKG_MGR $INSTALL_CMD"
 info "Package to be installed : $PKG_LIST"
 
-echo "Do you want to also install vim LSP dependencies : $EXTRA_PKG_LIST ?"
-select choice in "yes" "no"; do
-	case $choice in
-		yes) PKG_LIST+=$EXTRA_PKG_LIST ; break ;;
-		no) break ;;
-	esac
-done
 
 $SUDO_CMD $PKG_MGR $INSTALL_CMD $PKG_LIST
 echolor "Packages installed."
